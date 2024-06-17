cddd1a2e9094de7aa90272099ce43276e30eb7ac
Andrew Lavery <laverya@umich.edu>
2024-06-17 20:25:29 +1000

    add a field to the embedded cluster artifacts object to contain additional binaries

--- apis/kots/v1beta1/airgap_types.go
+++ apis/kots/v1beta1/airgap_types.go
@@ -44,11 +44,12 @@ type AirgapReleaseMeta struct {
 
 // EmbeddedClusterArtifacts maps embedded cluster artifacts to their path
 type EmbeddedClusterArtifacts struct {
-	Charts      string                  `json:"charts,omitempty"`
-	ImagesAmd64 string                  `json:"imagesAmd64,omitempty"`
-	BinaryAmd64 string                  `json:"binaryAmd64,omitempty"`
-	Metadata    string                  `json:"metadata,omitempty"`
-	Registry    EmbeddedClusterRegistry `json:"registry,omitempty"`
+	Charts              string                  `json:"charts,omitempty"`
+	ImagesAmd64         string                  `json:"imagesAmd64,omitempty"`
+	BinaryAmd64         string                  `json:"binaryAmd64,omitempty"`
+	Metadata            string                  `json:"metadata,omitempty"`
+	Registry            EmbeddedClusterRegistry `json:"registry,omitempty"`
+	AdditionalArtifacts map[string]string       `json:"additionalArtifacts,omitempty"`
 }
 
 // EmbeddedClusterRegistry holds a directory from where a images can be read and later
