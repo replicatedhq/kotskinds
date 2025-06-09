21506ee3d54b2a084b3f4b863b09cec22c55db38
Evans Mungai <evans@replicated.com>
2025-06-09 15:14:56 +0100

    chore: rename airgapUncompressedSize to uncompressedSize
    Signed-off-by: Evans Mungai <evans@replicated.com>

--- apis/kots/v1beta1/airgap_types.go
+++ apis/kots/v1beta1/airgap_types.go
@@ -31,7 +31,7 @@ type AirgapSpec struct {
 	RequiredReleases         []AirgapReleaseMeta       `json:"requiredReleases,omitempty"`
 	SavedImages              []string                  `json:"savedImages,omitempty"`
 	EmbeddedClusterArtifacts *EmbeddedClusterArtifacts `json:"embeddedClusterArtifacts,omitempty"`
-	AirgapUncompressedSize   int64                     `json:"airgapUncompressedSize,omitempty"`
+	UncompressedSize         int64                     `json:"uncompressedSize,omitempty"`
 	Format                   string                    `json:"format,omitempty"`
 	ReplicatedChartNames     []string                  `json:"replicatedChartNames,omitempty"`
 }
